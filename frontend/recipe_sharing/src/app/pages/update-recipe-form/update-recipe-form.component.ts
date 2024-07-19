dd55e6bbbdad2ddc0ac6cbc8ae7b6dd1679c620e
Rajamanthri <mcrajamanthri50@gmail.com>
2024-07-19 11:46:53 +0530

    recipe card created

---
+++ frontend/recipe_sharing/src/app/pages/update-recipe-form/update-recipe-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-recipe-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-recipe-form.component.html',
+  styleUrl: './update-recipe-form.component.scss'
+})
+export class UpdateRecipeFormComponent {
+
+}
